44bd3bf22c5b25f89a0521ad37bcb6243489808b
Talha Bin Nasir <talhaabinnasir@gmail.com>
2024-07-16 22:12:47 +0500

    Deploy static site

--- docs/static/rW_ImlgprA0ZvU7z_OUya/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{afterFiles:[],beforeFiles:[],fallback:[]},"/":["static/chunks/283-e8f8e8a281a255df.js","static/chunks/pages/index-1e7a2abdcb9d78f2.js"],"/_error":["static/chunks/pages/_error-ee5b5fb91d29d86f.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
