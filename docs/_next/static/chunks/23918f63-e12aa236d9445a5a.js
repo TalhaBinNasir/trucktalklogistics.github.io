c954c5c917131aca8037577ccbae4cdd4c2d1aad
TalhaBinNasir <talhaabinnasir@gmail.com>
2024-09-14 19:27:57 +0500

    add upload feature

---
+++ docs/_next/static/chunks/23918f63-e12aa236d9445a5a.js
@@ -0,0 +1,83 @@
+"use strict";(self.webpackChunk_N_E=self.webpackChunk_N_E||[]).push([[794],{2440:function(t,e,i){i.d(e,{NB:function(){return $7097090906378a5b$export$dc036a5afb9ca26f}});var s=i(5893);i(7294);var a=i(5480);let r={"aria-busy":!0,role:"progressbar"};(0,a.ZP).div`
+  display: ${t=>t.$visible?"flex":"none"};
+`;let n=(0,a.F4)`
+12.5% {
+  stroke-dasharray: ${33.98873199462888}px, ${242.776657104492}px;
+  stroke-dashoffset: -${26.70543228149412}px;
+}
+43.75% {
+  stroke-dasharray: ${84.97182998657219}px, ${242.776657104492}px;
+  stroke-dashoffset: -${84.97182998657219}px;
+}
+100% {
+  stroke-dasharray: ${2.42776657104492}px, ${242.776657104492}px;
+  stroke-dashoffset: -${240.34889053344708}px;
+}
+`;(0,a.ZP).path`
+  stroke-dasharray: ${2.42776657104492}px, ${242.776657104492};
+  stroke-dashoffset: 0;
+  animation: ${n} ${1.6}s linear infinite;
+`;let o=(0,a.F4)`
+to {
+   transform: rotate(360deg);
+ }
+`;(0,a.ZP).svg`
+  animation: ${o} 0.75s steps(12, end) infinite;
+  animation-duration: 0.75s;
+`,(0,a.ZP).polyline`
+  stroke-width: ${t=>t.width}px;
+  stroke-linecap: round;
+
+  &:nth-child(12n + 0) {
+    stroke-opacity: 0.08;
+  }
+
+  &:nth-child(12n + 1) {
+    stroke-opacity: 0.17;
+  }
+
+  &:nth-child(12n + 2) {
+    stroke-opacity: 0.25;
+  }
+
+  &:nth-child(12n + 3) {
+    stroke-opacity: 0.33;
+  }
+
+  &:nth-child(12n + 4) {
+    stroke-opacity: 0.42;
+  }
+
+  &:nth-child(12n + 5) {
+    stroke-opacity: 0.5;
+  }
+
+  &:nth-child(12n + 6) {
+    stroke-opacity: 0.58;
+  }
+
+  &:nth-child(12n + 7) {
+    stroke-opacity: 0.66;
+  }
+
+  &:nth-child(12n + 8) {
+    stroke-opacity: 0.75;
+  }
+
+  &:nth-child(12n + 9) {
+    stroke-opacity: 0.83;
+  }
+
+  &:nth-child(12n + 11) {
+    stroke-opacity: 0.92;
+  }
+`;let l=(0,a.F4)`
+to {
+   stroke-dashoffset: 136;
+ }
+`;(0,a.ZP).polygon`
+  stroke-dasharray: 17;
+  animation: ${l} 2.5s cubic-bezier(0.35, 0.04, 0.63, 0.95) infinite;
+`,(0,a.ZP).svg`
+  transform-origin: 50% 65%;
+`;let $7097090906378a5b$export$dc036a5afb9ca26f=({visible:t=!0,width:e="80",height:i="80",colors:a=["#e15b64","#f47e60","#f8b26a","#abbd81","#849b87"],wrapperClass:n="",wrapperStyle:o={},ariaLabel:l="color-ring-loading"})=>t?(0,s.jsxs)("svg",{xmlns:"http://www.w3.org/2000/svg",xmlnsXlink:"http://www.w3.org/1999/xlink",width:e,height:i,viewBox:"0 0 100 100",preserveAspectRatio:"xMidYMid",className:n,style:o,"aria-label":l,"data-testid":"color-ring-svg",...r,children:[(0,s.jsx)("defs",{children:(0,s.jsx)("mask",{id:"ldio-4offds5dlws-mask",children:(0,s.jsx)("circle",{cx:"50",cy:"50",r:"26",stroke:"#fff",strokeLinecap:"round",strokeDasharray:"122.52211349000194 40.840704496667314",strokeWidth:"9",transform:"rotate(198.018 50 50)",children:(0,s.jsx)("animateTransform",{attributeName:"transform",type:"rotate",values:"0 50 50;360 50 50",keyTimes:"0;1",dur:"1s",repeatCount:"indefinite"})})})}),(0,s.jsxs)("g",{mask:"url(#ldio-4offds5dlws-mask)",children:[(0,s.jsx)("rect",{x:"14.5",y:"0",width:"15",height:"100",fill:a[0],children:(0,s.jsx)("animate",{attributeName:"fill",values:a.join(";").toString(),keyTimes:"0;0.25;0.5;0.75;1",dur:"1s",repeatCount:"indefinite",begin:"-0.8s"})}),(0,s.jsx)("rect",{x:"28.5",y:"0",width:"15",height:"100",fill:a[1],children:(0,s.jsx)("animate",{attributeName:"fill",values:a.join(";").toString(),keyTimes:"0;0.25;0.5;0.75;1",dur:"1s",repeatCount:"indefinite",begin:"-0.6s"})}),(0,s.jsx)("rect",{x:"42.5",y:"0",width:"15",height:"100",fill:a[2],children:(0,s.jsx)("animate",{attributeName:"fill",values:a.join(";").toString(),keyTimes:"0;0.25;0.5;0.75;1",dur:"1s",repeatCount:"indefinite",begin:"-0.4s"})}),(0,s.jsx)("rect",{x:"56.5",y:"0",width:"15",height:"100",fill:a[3],children:(0,s.jsx)("animate",{attributeName:"fill",values:a.join(";").toString(),keyTimes:"0;0.25;0.5;0.75;1",dur:"1s",repeatCount:"indefinite",begin:"-0.2s"})}),(0,s.jsx)("rect",{x:"70.5",y:"0",width:"15",height:"100",fill:a[4],children:(0,s.jsx)("animate",{attributeName:"fill",values:a.join(";").toString(),keyTimes:"0;0.25;0.5;0.75;1",dur:"1s",repeatCount:"indefinite",begin:"0s"})})]})]}):null}}]);
